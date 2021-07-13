5042dee42dbe941257a353ecc716d39d9d78783d
Smrutiranjan Sahu <srsahu13@gmail.com>
2021-07-13 14:20:02 +0530

    'updated'

--- client/.next/static/webpack/webpack.1c34537fed656b1d5697.hot-update.js
+++ client/.next/static/webpack/webpack.1c34537fed656b1d5697.hot-update.js
@@ -4,9 +4,9 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a4f5cc64e3e3594b5474"; }
+/******/ 	__webpack_require__.h = function() { return "7a7e1751768bfd6d17b7"; }
 /******/ }();
 /******/ 
 /******/ }
 );
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay4xYzM0NTM3ZmVkNjU2YjFkNTY5Ny5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcImE0ZjVjYzY0ZTNlMzU5NGI1NDc0XCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay4xYzM0NTM3ZmVkNjU2YjFkNTY5Ny5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjdhN2UxNzUxNzY4YmZkNmQxN2I3XCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
